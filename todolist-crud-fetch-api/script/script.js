a0143d72df0e9c573043db764f3c6c644914bf6b
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-30 17:47:50 +0300

    Update script.js

--- todolist-crud-fetch-api/script/script.js
+++ todolist-crud-fetch-api/script/script.js
@@ -1,6 +1,6 @@
 const input = document.querySelector(".task-info-input");
 const description = document.querySelector(".task-type-input");
-const due_date = document.querySelector(".task-date-input");
+const deadline = document.querySelector(".task-date-input");
 const addButton = document.querySelector(".task-add");
 const showunfulfilledButton = document.querySelector(".incompleted");
 const showAllButton = document.querySelector(".all");
@@ -9,303 +9,260 @@ const todoList = document.querySelector(".list-flex");
 const check = document.querySelector(".list-item-checkbox");
 const listValue = document.querySelector(".list-item-value");
 
-let slate = [];
-let url = 'http://localhost:3000/tasks'
+let state = [];
+let URL = "http://localhost:3000/tasks";
+// showunfulfilledButton.addEventListener("click", showUnfulfilled);
+// function showUnfulfilledTodos() {}
+// showAllButton.addEventListener("click", showAll);
+// function showAllTodos() {}
 
-addButton.addEventListener("click", createItem);
-showunfulfilledButton.addEventListener("click", showUnfulfilled);
-showAllButton.addEventListener("click", showAll);
+addButton.addEventListener("submit", addTodo);
 
-function render() {
-    if (slate.length == 0) {
-        setListHeader() 
-    } else {
-        resetPlaceholder()
-        createBoard()
-    }
+function addTodo() {
+  e.preventDefault();
+  isEmptyTitle();
+  buildTodoItem();
+  setState();
+  createBoard();
+  writeTodo();
 }
- 
-
-function getTodosList() {
-    return getData()
-    .then(date => {
-        return date;
-    });
-}
-   
-function createItem() {
-    let typeBC = ["#ffecb5", "#e2bbff", "#b6ffee", "#ffb4c0", "#bbfaff"];
-    if (input.value === "") {
-        return alert("Empty title!");
-    }
-    const todo = {
-        value: input.value,
-        done: false,
-        description: description.value,
-        deadline: due_date.value ? new Date(due_date.value).toLocaleDateString() : "",
-        labelColor: typeBC[Math.floor(Math.random() * typeBC.length)]
-    };
-    description.value = "";
-    input.value = "";
-    due_date.value = "";
-    listHdr.innerHTML = "Add a task to begin.";
-    todoList.innerHTML = "";
-    slate.unshift(todo);
-    createBoard();
-    saveTodoOnServer(todo)
-}
-
 
 function createBoard() {
-    slate.forEach(createSingleTodo);
+  state.forEach(createSingleTodo);
 }
 
-function createSingleTodo(el) {
-    const todo = document.createElement("div"),
-    checkbox = document.createElement('input'),
-        titleDiv = document.createElement("div"),
-        descDiv = document.createElement("div"),
-        dateDiv = document.createElement("div"),
-        RemoveTaskById = document.createElement("div");
-    
-    todoList.appendChild(todo);
-    printTodo(todo, el);
-
-    todo.appendChild(checkbox);
-    createCheckbox(checkbox, el);
-
-    todo.appendChild(titleDiv);
-    printTitleDiv(titleDiv, el);
-
-    todo.appendChild(descDiv);
-    createRemoveTaskById(RemoveTaskById);
-
-    todo.appendChild(dateDiv);
-    printDescDiv(descDiv, el);
-
-    todo.appendChild(RemoveTaskById);
-    createDateDiv(dateDiv, el);
-
-    RemoveTaskById.onclick = RemoveDiv
-    checkbox.onclick = setDoneState
-    return todo;
-}
-
-// function createSingleTodo(todoItem) {
-//     const todoNode = document.createElement("div");
-//     todoNode.appendChild(
-//         createTodo(todoItem)
-//         ,createCheckbox(todoItem)
-//         ,createTitle(todoItem)
-//         ,createRemoveTaskById(todoItem)
-//         ,createDesc(todoItem)
-//         ,createDate(todoItem)
-//     )
-//     return todoNode;
-// }
-
-function createTodo(parentDiv, el){
-    parentDiv.classList.add("list-item");
-    parentDiv.id = el.id;
-}
-
-async function setDoneState() {
-    let div = this.parentNode;
-    let id = this.parentNode.id
-    let parent = div.parentNoderes
-    return await getItemFromServer(id)
-        .then((todo) => {
-            return fetch(url + `/` + id, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    done: !todo.done
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8'
-                },
-            })
-        })
-        .then(todo => todo.json())
-        .then((todo) => {
-            // console.log(createSingleTodo(todo));
-            parent.replaceChild(createSingleTodo(todo), div)
-        })
-}
-
-function deleteItem(id) {
-    const task = slate.find(task => task.id === id)
-    if (task !== -1) {
-        slate.splice(task, 1);  
-    }
-    return fetch(url + `/` + id, {
-        method: 'DELETE',
-    }).then(res => res.json())
-}
-
- function RemoveDiv() {
-    // console.log(this.parentNode.id)
-    let div = this.parentNode;
-    let id = this.parentNode.id
-    return fetch(url + `/` + id, {
-        method: 'DELETE',
-    }).then(response => response.json())
-}
-
-
-function createCheckbox(el) {
-    checkbox = document.createElement('input')
-    if (el.done === true) {
-        checkbox.checked = true;
-    }
-    checkbox.type = "checkbox";
-    checkbox.classList.add("list-item-check");
-}
-
-function printTitleDiv(title, el) {
-    if (el.done) {
-        title.style.color = '#505050';
-        title.style.textDecoration = 'line-through';
-    }
-    title.classList.add("list-item-value");
-    title.textContent = `${el.value}`;
-}
-
-function createRemoveTaskById(RemoveTaskById) {
-    RemoveTaskById.classList.add("list-item-remove");
-    RemoveTaskById.innerHTML = `<img src="assets/icons/delete.svg" />`;
-}
-
-function printDescDiv(item, el) {
-    if (el.description === "") {
-        item.style.display = `none`
-    } else if (el.description !== "") {
-        item.style.display = `flex`
-        item.classList.add("list-item-type");
-        item.style.backgroundColor = el.labelColor;
-        item.style.color = '#242323';
-        item.textContent = `${el.description}`;
-    }
-}
-
-function createDateDiv(item, el) {
-    if (isDueDate(el.deadline)) {
-        item.classList.add("list-item-date-gone");
-    } else {
-        item.classList.add("list-item-date");
-    }
-    item.textContent = `${el.deadline}`
+const buildTodoItem = () => {
+  return (todo = {
+    value: input.value,
+    done: false,
+    description: description.value,
+    deadline: deadline.value
+      ? new Date(deadline.value).toLocaleDateString()
+      : "",
+    labelColor: genColor(),
+  });
 };
 
-function isDueDate(date) {
-    let itemDate = new Date(date);
-    console.log(itemDate);
-    const currentDate = new Date();
-    console.log(currentDate)
-    return itemDate <= currentDate; // <= for today
+function createSingleTodo(todoItem) {
+  let todoNode = document.createElement("div");
+  todoNode.id = todoItem.id;
+  todoNode.classList.add("list-item");
+  todoList.appendChild(todoNode);
+  todoNode.append(
+    createCheckbox(todoItem),
+    createTitle(todoItem),
+    createDescription(todoItem),
+    createDate(todoItem),
+    createDeleteButton(todoItem)
+  );
+  return todoNode;
+  // todoNode.appendChild(createCheckbox(todoItem));
+  // todoNode.appendChild(createTitle(todoItem));
+  // todoNode.appendChild(createDeleteButton(todoItem));
+  // todoNode.appendChild(createDescription(todoItem));
+  // todoNode.appendChild(createDate(todoItem));
 }
 
-function resetPlaceholder() {
-    todoList.innerHTML = '';
+const createCheckbox = (todo) => {
+  let checkbox = document.createElement("input");
+  if (todo.done) {
+    checkbox.checked = true;
+  }
+  checkbox.type = "checkbox";
+  checkbox.classList.add("list-item-check");
+  checkbox.onclick = setDoneState;
+
+  return checkbox;
+};
+
+const createTitle = (todo) => {
+  let title = document.createElement("div");
+  if (todo.done) {
+    title.style.color = "#505050";
+    title.style.textDecoration = "line-through";
+  }
+  title.classList.add("list-item-value");
+  title.textContent = `${todo.value}`;
+
+  return title;
+};
+const createDeleteButton = () => {
+  let removeButton = document.createElement("div");
+  removeButton.classList.add("list-item-remove");
+  removeButton.innerHTML = `<img src="assets/icons/delete.svg" />`;
+  removeButton.onclick = deleteTodo;
+
+  return removeButton;
+};
+
+const createDescription = (todo) => {
+  let description = document.createElement("div");
+  if (!todo.description) {
+    description.style.display = `none`;
+  } else if (todo.description) {
+    description.style.display = `flex`;
+    description.classList.add("list-item-type");
+    description.style.backgroundColor = todo.labelColor;
+    description.style.color = "#242323";
+    description.textContent = `${todo.description}`;
+  }
+  return description;
+};
+function createDate(todo) {
+  let dueDate = document.createElement("div");
+  if (isDueDate(todo.deadline)) {
+    dueDate.classList.add("list-item-date-gone");
+  } else {
+    dueDate.classList.add("list-item-date");
+  }
+  dueDate.textContent = `${todo.deadline}`;
+  return dueDate;
 }
 
+const isDueDate = (date) => {
+  let itemDate = new Date(date);
+  console.log(itemDate);
+  const currentDate = new Date();
+  console.log(currentDate);
+  return itemDate <= currentDate; // <= for today
+};
 
-async function showAll() {
-    return await getData().then(res => slate = res)
-    .then(() => render())
+const isEmptyTitle = () => {
+  if (input.value === "") {
+    return alert("Empty title!");
+  }
+};
+
+function setState() {
+  return slate.push(todo);
 }
 
-async function showUnfulfilled() {
-    return await getData().then(res => {
-            slate = res.filter(task => task.done === false);
-        }).then(() => render())
-}
+const genColor = () => {
+  let typeBC = ["#ffecb5", "#e2bbff", "#b6ffee", "#ffb4c0", "#bbfaff"];
+  return (laberlColor = typeBC[Math.floor(Math.random() * typeBC.length)]);
+};
 
-async function getData() {
-    const res = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
+async function getTodoById(id) {
+  return fetch(URL + `/` + id)
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
     });
-    return await res.json();
-}
-      
-
-function getItemFromServer(id) {
-    return fetch(url + `/` + id)
-            .then(res => res.json())
-            .then(json => {
-                return json;
-            })
 }
 
-function saveTodoOnServer(item) {
-    return  fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(item),
-    }).then(todo => todo.json());
+async function getAllTodos() {
+  const res = await fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return await res.json();
+}
+async function showAll() {
+  return await getAllTodos()
+    .then((res) => (slate = res))
+    .then(() => render());
 }
 
-function saveItemState(id) {
-    return getItemFromServer(id)
-        .then((todo) => {
-            return fetch(url + `/` + id, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    done: !todo.done
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8'
-                },
-            })
-        })
-    }
+async function deleteTodo() {
+  let div = this.parentNode;
 
-async function getTodoListApi() {
-    let res;
-    let array;
-    try {
-        res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(`Error: ${res.status}`);
-        } else {
-            array = await res.json();
+  return await getItemFromServer(div.id).then((todo) => {
+    return fetch(URL + `/` + div.id, {
+      method: "DELETE",
+      body: JSON.stringify({
+        todo: todo,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+    .then((res) => res.json())
+    .then((task) => {
+      if (res.ok) {
+        const task = slate.find((task) => task.id === id);
+        if (task !== -1) {
+          slate.splice(task, 1);
         }
-    } catch (e) {
-        console.log(e);
-    }
-    return array;
+      }
+    })
+})
+
+async function setDoneState() {
+  let div = this.parentNode;
+  return await getItemFromServer(div.id)
+    .then((todo) => {
+      return fetch(URL + `/` + div.id, {
+        method: "PATCH",
+        body: JSON.stringify({
+          done: !todo.done,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+    })
+    .then((todo) => todo.json())
+    .then((todo) => {
+      div.parentElement.replaceChild(todo, div);
+    });
+}
+function getItemFromServer(id) {
+  return fetch(URL + `/` + id)
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    });
 }
 
-showAll();
-input.focus();
+function writeTodo(todo) {
+  return fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  }).then((response) => response.json());
+}
 
+async function getTodosList() {
+  let response;
+  let result;
+  try {
+    response = await fetch(URL);
+    if (!res.ok) {
+      throw new Error(`Error: ${res.status}`);
+    } else {
+      result = await response.json();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return result;
+}
 
-// function setDoneState() {
-//     let div = this.parentNode;
-//     let checkbox = div.children[0]
-//     let title = div.children[1];
-//     if (checkbox.checked) {
-//         title.style.color = '#505050';
-//         title.style.textDecoration = 'line-through';
-//             } else {
-//                 title.style.color = '#b8b8b8';
-//                 title.style.textDecoration = 'none';
-//     }
-//     let id = this.parentNode.id
-//     return getItemFromServer(id)
-//         .then((todo) => {
-//             return fetch(url + `/` + id, {
-//                 method: 'PATCH',
-//                 body: JSON.stringify({
-//                     done: !todo.done
-//                 }),
-//                 headers: {
-//                     'Content-type': 'application/json; charset=UTF-8'
-//                 },
-//             })
-//         })
+getAllTodos()
+  .then((res) => (state = res))
+  .then(() => createBoard());
+
+// const createTodoNode = () => {
+//   let todoNode = document.createElement("div");
+//   todoList.appendChild(todoNode);
+//   todoNode.classList.add("list-item");
+//   return todoNode;
+// };
+// return fetch('/contacts', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' }
+//     body: JSON.stringify(contact)
+//     })
+//     .then(res => res.ok ? res.json() : throw new Error(res))
+
+//     event.preventDefault();
+// createContact(contact)
+// .then(c => contactListNode.appendChild(createContactNode(c)));
+// function createTodoItem(parentDiv, el) {
+//   parentDiv.classList.add("list-item");
+//   parentDiv.id = el.id;
 // }
